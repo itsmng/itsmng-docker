e0ff61dd498ef21ca69eea6745a245e47592dd07
Florian Blanchet <contact@minzord.fr>
2023-01-24 09:58:33 +0100

    add HostName Variable

--- 1.3.0/docker-entrypoint.d/10-postinstall.sh
+++ 1.3.0/docker-entrypoint.d/10-postinstall.sh
@@ -4,7 +4,7 @@ if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
 	sleep 7
 	cd /var/www/itsm-ng/files/
 	mkdir _cache _cron _dumps _graphs _locales _lock _log _pictures _plugins _rss _sessions _tmp _uploads
-	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H itsmdb -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
+	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
 	chown -R apache:apache /var/www/itsm-ng/files
 	rm /var/www/itsm-ng/install/install.php
 fi
