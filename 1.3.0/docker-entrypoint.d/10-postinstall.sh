f3fec93fbfa2730ff52126680b42f18044ad7516
Florian Blanchet <contact@minzord.fr>
2023-01-24 21:54:20 +0100

    fix directory of postinstall

--- 1.3.0/docker-entrypoint.d/10-postinstall.sh
+++ 1.3.0/docker-entrypoint.d/10-postinstall.sh
@@ -2,9 +2,7 @@
 
 if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
 	sleep 5
-	cd /var/www/itsm-ng/files/
-	mkdir _cache _cron _dumps _graphs _locales _lock _log _pictures _plugins _rss _sessions _tmp _uploads
 	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
-	chown -R apache:apache /var/www/itsm-ng/files
+	chown -R www-data:www-data /var/www/itsm-ng/files
 	rm /var/www/itsm-ng/install/install.php
 fi
