27486819d38f001fd2acfbe93bee389a57afff1a
Florian Blanchet <contact@minzord.fr>
2023-01-26 09:47:53 +0100

    Add 1.4 version of ITSM-NG

---
+++ 1.4.0/docker-entrypoint.d/10-postinstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
+	sleep 5
+	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
+	chown -R www-data:www-data /var/www/itsm-ng/files
+	rm /var/www/itsm-ng/install/install.php
+fi
