3b2181d7c4d75834ee0b32c4847a9eaa6f032b35
Florian Blanchet <florian.blanchet@itsm-ng.com>
2024-12-06 15:38:32 +0100

    fix: create lib folder

--- latest/docker-entrypoint.d/10-postinstall.sh
+++ latest/docker-entrypoint.d/10-postinstall.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
+        mkdir -p /var/www/itsm-ng/files/{_cache,_cron,_dumps,_graphs,_lock,_pictures,_plugins,_rss,_sessions,_tmp,_uploads}
+	chown -R www-data:www-data /var/www/itsm-ng/files
+fi
+
+if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
 	sleep 5
 	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
 	chown -R www-data:www-data /var/www/itsm-ng/files
