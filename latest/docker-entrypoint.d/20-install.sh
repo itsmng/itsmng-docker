43689a950206284c6d061488e938199f00fd75f2
Florian Blanchet <florian.blanchet@itsm-ng.com>
2024-12-09 13:26:15 +0100

    fix

--- latest/docker-entrypoint.d/20-install.sh
+++ latest/docker-entrypoint.d/20-install.sh
@@ -8,5 +8,6 @@ done
 
 # Run the installation if config_db.php doesn't exist
 if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
+	chown -R www-data:www-data /var/www/itsm-ng/files/
 	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
 fi
