2ef1418fc55fb5ff68fa4953e92392553c8d0cb2
Florian Blanchet <florian.blanchet@itsm-ng.com>
2024-12-09 11:37:35 +0100

    feat: wait if mysql server is down

--- latest/docker-entrypoint.d/20-install.sh
+++ latest/docker-entrypoint.d/20-install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Wait for MySQL to be ready
+until nc -z -v -w30 $MARIADB_HOST 3306; do
+	echo "Waiting for MySQL connection..."
+	sleep 2
+done
+
+# Run the installation if config_db.php doesn't exist
 if [ ! -f /var/www/itsm-ng/config/config_db.php ]; then
-	sleep 5
 	cd /var/www/itsm-ng && php bin/console itsmng:database:install -H $MARIADB_HOST -u $MARIADB_USER -p $MARIADB_PASSWORD -d $MARIADB_DATABASE -n
 fi
--- latest/docker-entrypoint.d/30-update.sh
+++ latest/docker-entrypoint.d/30-update.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Wait for MySQL to be ready
+until nc -z -v -w30 $MARIADB_HOST 3306; do
+	echo "Waiting for MySQL connection..."
+	sleep 2
+done
+
+# Run the update if config_db.php exist
 if [ -f /var/www/itsm-ng/config/config_db.php ]; then
-	sleep 5
 	cd /var/www/itsm-ng && php bin/console itsmng:database:update -n
 fi
