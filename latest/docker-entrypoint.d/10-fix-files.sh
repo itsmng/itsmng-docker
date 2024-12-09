ca2c0d201d1a1d526fd9b235aa079568537fbb8a
Florian Blanchet <florian.blanchet@itsm-ng.com>
2024-12-09 13:59:06 +0100

    fix:

--- latest/docker-entrypoint.d/10-fix-files.sh
+++ latest/docker-entrypoint.d/10-fix-files.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 if [ ! -d /var/www/itsm-ng/files/_cache ]; then
-        mkdir -pv /var/www/itsm-ng/files/{_cache,_cron,_dumps,_graphs,_lock,_pictures,_plugins,_rss,_sessions,_tmp,_uploads}
+        mkdir -pv /var/www/itsm-ng/files/_cache 	\
+		/var/www/itsm-ng/files/_cron 		\
+		/var/www/itsm-ng/files/_dumps 		\
+		/var/www/itsm-ng/files/_graphs 		\
+		/var/www/itsm-ng/files/_lock 		\
+		/var/www/itsm-ng/files/_pictures 	\
+		/var/www/itsm-ng/files/_plugins 	\
+		/var/www/itsm-ng/files/_rss 		\
+		/var/www/itsm-ng/files/_sessions 	\
+		/var/www/itsm-ng/files/_tmp 		\
+		/var/www/itsm-ng/files/_uploads
 	
 	chown -R www-data:www-data /var/www/itsm-ng/files
 fi
