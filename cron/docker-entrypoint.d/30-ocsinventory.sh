8080074161b5aa14b4fec7b4b4dc59921eab8235
Florian Blanchet <florian.blanchet@itsm-ng.com>
2024-12-11 11:54:08 +0100

    fix: condition

--- cron/docker-entrypoint.d/30-ocsinventory.sh
+++ cron/docker-entrypoint.d/30-ocsinventory.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ ! -f /var/www/itsm-ng/plugins/ocsinventoryng/scripts/run.php ]; then
+if [ -f /var/www/itsm-ng/plugins/ocsinventoryng/scripts/run.php ]; then
 	php /var/www/itsm-ng/plugins/ocsinventoryng/scripts/run.php
 fi
